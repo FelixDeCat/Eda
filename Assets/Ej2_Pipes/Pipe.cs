5ba77c00d1aad303092250166bf096f252223c33
felix-PC\felix <guillermofelixromero@gmail.com>
2018-06-04 23:05:31 -0300

    asd

--- Assets/Ej2_Pipes/Pipe.cs
+++ Assets/Ej2_Pipes/Pipe.cs
@@ -32,6 +32,34 @@ public class Pipe : MonoBehaviour {
 		gameObject.transform.localPosition = new Vector3(_x*3f, -_y*3f, 0f);
 	}
 
+
+    // eesto esta para ayudar a un amigo con una tarea de C... no lo borre xq por ahi me lo pide denuevo
+    //void main()
+    //{
+    //    int val1 = validar_dato(-19, 240);
+    //    int val2 = validar_dato(-19, 240);
+    //    int val3 = validar_dato(-19, 240);
+
+    //    //print bla bla bla
+    //}
+
+    //int valor; 
+    //int validar_dato(int a, int b)
+    //{
+    //    do
+    //    {
+    //        printf("bla bla bla");
+    //        scanf("%d", &valor);
+    //    }
+    //    while (esdatoValido(valor, a, b));
+    //    return valor;
+    //}
+
+    //bool esdatoValido(int x, int lim1, int lim2) {
+    //    if (x > lim1 && x < lim2) return true;
+    //    else return false;
+    //}
+
 	//Asigna un material al modelo
 	public Material material
     {
@@ -42,6 +70,8 @@ public class Pipe : MonoBehaviour {
 				p.GetComponent<Renderer>().material = _material;
 		}
 	}
+
+
 
 	//Hace visible o invisible una conexion (representacion grafica unicamente)
 	//Ejemplo: SetConnection(Direction.Left, true);
